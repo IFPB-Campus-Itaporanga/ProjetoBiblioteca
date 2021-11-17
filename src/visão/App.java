d9219f9d8c6aa4aba666c7e7366f3a3c498eed45
Douglas <douglasf.xavier@gmail.com>
2021-11-17 07:41:13 -0300

    Correção de bugs e refatoração do código

--- src/visão/App.java
+++ src/visão/App.java
@@ -3,48 +3,50 @@ package visão;
 import controlador.AcervoController;
 import modelo.Acervo;
 import modelo.Livro;
-
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
-        String tituloLivro, autorLivro, editoraLivro;
         int anoPublicaoLivro;
+        char comando;
+        String tituloLivro, autorLivro, editoraLivro;
         Livro livro;
         AcervoController acervoController = new AcervoController();
         Acervo acervoAtual;
-        String comando;
 
+        Scanner leitor = new Scanner(System.in);
 
         do {
 
             System.out.println("Digite o título do livro:");
             tituloLivro = leitor.nextLine();
 
-            System.out.println("Digite o nome do autor");
+            System.out.println("Digite o nome do autor:");
             autorLivro = leitor.nextLine();
 
-            System.out.println("Digite o nome da editora");
+            System.out.println("Digite o nome da editora:");
             editoraLivro = leitor.nextLine();
 
-            System.out.println("Digite o ano de publicação");
+            System.out.println("Digite o ano de publicação:");
             anoPublicaoLivro = leitor.nextInt();
 
             livro = new Livro(tituloLivro,anoPublicaoLivro,autorLivro,editoraLivro);
 
             acervoAtual = acervoController.cadastrarLivro(livro);
 
-            System.out.println("Cadastar outro livro?");
-            comando = leitor.nextLine();
+            System.out.println("Cadastar outro livro?(1-SIM,0-NÃO):");
+            comando = leitor.next().charAt(0);
+            leitor.nextLine();
 
-        }while(comando.equals("YES"));
+        }while(comando == '1');
 
-        System.out.println("*************************************");
+        leitor.close();
+
+        System.out.println("\n*************************************");
         System.out.println("LIVROS DA DOUGLATECA");
         System.out.println("*************************************");
         for(Livro itemLivro: acervoAtual.getLivros()){
-            System.out.println("Título: " + itemLivro.getTituloLivro());
+            System.out.println("\nTítulo: " + itemLivro.getTituloLivro());
             System.out.println("Autor: " + itemLivro.getAutorLivro());
             System.out.println("Data de publicação: " + itemLivro.getAnoPublicacao());
             System.out.println("Editora: " + itemLivro.getEditoraLivro());
